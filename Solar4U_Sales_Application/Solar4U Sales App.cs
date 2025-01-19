ae2b9850ead57038944b35bf8b83ec4ec9a551fe
Sohamtam46 <sohamtam46@gmail.com>
2025-01-19 08:56:21 +0000

    updates to description

--- Solar4U_Sales_Application/Solar4U Sales App.cs
+++ Solar4U_Sales_Application/Solar4U Sales App.cs
@@ -1,14 +1,11 @@
 ﻿/* 
- * Student Name: Soham Tambde
- * Student ID: 24259687
+ * Name: Soham Tambde
  * Date: 18/11/2024
- * Assignment: 3
- * Assignment: The Application allows the Business User to first take in the order details and then take Customer Data.
+ * Description: The Application allows the Business User to first take in the order details and then take Customer Data.
  *             After confirming Customers Data and their order details, User is given a proper view of the loan options available.
  *             User can choose of the three available financing option or go with no loan needed and then confirm order. The Order 
  *             Details are saved in a text file which can be then looked up in the Search functionality of the application. Search 
  *             can be perfomed either by Transaction number or Transaction Date.
- * 
  */
 
 using System;
